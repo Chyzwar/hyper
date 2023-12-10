7b8168cb8bcceac77f5266417a39cb99b88ca7b5
chyzwar <schizek.marcin@gmail.com>
2023-12-10 22:18:09 +0100

    chore(): more unit tests tweaks

--- jest.unit.js
+++ jest.unit.js
@@ -1,12 +1,11 @@
 
 
-process.env.NODE_OPTIONS = "--experimental-vm-modules";
+process.env.NODE_OPTIONS = "--experimental-vm-modules --no-warnings";
 
 export default {
   verbose: false,
   transform: {},
   projects: [
-    "<rootDir>/packages/*/jest.config.js",
     "<rootDir>/packages/*/jest.config.ts",
   ],
 };
--- packages/http/jest.config.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-  displayName: "@hyper/http",
-  testEnvironment: "node",
-  transform: {},
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|jsx?)$",
-  roots: ["<rootDir>/lib/"],
-};
-
